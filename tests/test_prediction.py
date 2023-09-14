a6537e182fea69a900e22528a83e3ecbda893f31
Viktor Bulatov <bt.uytya@gmail.com>
2023-09-14 23:00:09 +0300

    add test data

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -2,6 +2,12 @@ import pytest
 
 import dawg_python
 from .utils import data_path
+from hashlib import md5
+
+
+def encode(w):
+    code = md5(w.encode('utf8'))
+    return tuple([ord(c) for c in code.hexdigest()])[:4]
 
 
 class TestPrediction:
@@ -63,11 +69,14 @@ class TestPrediction:
         d = self.record_dawg()
         assert d.similar_item_values(word, self.REPLACES) == prediction
 
+
 class TestMultiValuedPrediction(object):
 
     REPLACES = dawg_python.DAWG.compile_replaces({'е': ['ё', 'ѣ'], 'и': 'і'})
 
     DATA = "хлѣб ёлка ель лѣс лѣсное всё всѣ бѣлёная изобрѣтён лев лёв лѣв вѣнскій".split(" ")
+    # STORED_DATA = list(zip(DATA, (encode(w) for w in DATA)))
+
     SUITE = [
         ('осел', []),
         ('ель', ['ель']),
@@ -93,7 +102,7 @@ class TestMultiValuedPrediction(object):
         (
             it[0], # key
             [
-                (w, [(len(w),)]) # item, value pair
+                (w, [encode(w)]) # item, value pair
                 for w in it[1]
             ]
         )
@@ -103,18 +112,18 @@ class TestMultiValuedPrediction(object):
     SUITE_VALUES = [
         (
             it[0], # key
-            [[(len(w),)] for w in it[1]]
+            [[encode(w)] for w in it[1]]
         )
         for it in SUITE
     ]
 
     def record_dawg(self):
-        path = data_path("small", "prediction-record.dawg")
-        return dawg_python.RecordDAWG(str("=H")).load(path)
+        path = data_path("small", "prediction1917-record.dawg")
+        return dawg_python.RecordDAWG(str("=HHHH")).load(path)
 
     @pytest.mark.parametrize(("word", "prediction"), SUITE)
     def test_dawg_prediction(self, word, prediction):
-        d = dawg_python.DAWG().load(data_path("small", "prediction.dawg"))
+        d = dawg_python.DAWG().load(data_path("small", "prediction1917.dawg"))
         assert d.similar_keys(word, self.REPLACES) == prediction
 
     @pytest.mark.parametrize(("word", "prediction"), SUITE)
