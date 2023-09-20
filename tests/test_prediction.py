21bd8bbd94c60a003f190bcd01b277715f15db82
bt2901 <BT_2901_42@mail.ru>
2023-09-20 09:44:33 +0300

    nondeterministic char_substitutes (PR port) (#4)
    * try to port to a fork in a less fancy way
    
    * fix merge artifacts
    
    * fix some more merge artifacts
    
    * fix similar_items_values
    
    * add test data
    
    * remove commented code
    
    * add sources for binary files

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -2,6 +2,12 @@ import pytest
 
 import dawg_python
 from .utils import data_path
+from hashlib import md5
+
+
+def encode(w):
+    code = md5(w.encode('utf8'))
+    return tuple([ord(c) for c in code.hexdigest()])[:4]
 
 
 class TestPrediction:
@@ -46,6 +52,77 @@ class TestPrediction:
     @pytest.mark.parametrize(("word", "prediction"), SUITE)
     def test_dawg_prediction(self, word, prediction):
         d = dawg_python.DAWG().load(data_path("small", "prediction.dawg"))
+        assert d.similar_keys(word, self.REPLACES) == prediction
+
+    @pytest.mark.parametrize(("word", "prediction"), SUITE)
+    def test_record_dawg_prediction(self, word, prediction):
+        d = self.record_dawg()
+        assert d.similar_keys(word, self.REPLACES) == prediction
+
+    @pytest.mark.parametrize(("word", "prediction"), SUITE_ITEMS)
+    def test_record_dawg_items(self, word, prediction):
+        d = self.record_dawg()
+        assert d.similar_items(word, self.REPLACES) == prediction
+
+    @pytest.mark.parametrize(("word", "prediction"), SUITE_VALUES)
+    def test_record_dawg_items_values(self, word, prediction):
+        d = self.record_dawg()
+        assert d.similar_item_values(word, self.REPLACES) == prediction
+
+
+class TestMultiValuedPrediction(object):
+
+    REPLACES = dawg_python.DAWG.compile_replaces({'е': ['ё', 'ѣ'], 'и': 'і'})
+
+    DATA = "хлѣб ёлка ель лѣс лѣсное всё всѣ бѣлёная изобрѣтён лев лёв лѣв вѣнскій".split(" ")
+
+    SUITE = [
+        ('осел', []),
+        ('ель', ['ель']),
+        ('ёль', []),
+        ('хлеб', ['хлѣб']),
+        ('елка', ['ёлка']),
+        ('лесное', ['лѣсное']),
+        ('лесноё', []),
+        ('лёсное', []),
+        ('изобретен', ['изобрѣтён']),
+        ('беленая', ['бѣлёная']),
+        ('белёная', ['бѣлёная']),
+        ('бѣленая', ['бѣлёная']),
+        ('бѣлёная', ['бѣлёная']),
+        ('белѣная', []),
+        ('бѣлѣная', []),
+        ('все', ['всё', 'всѣ']),
+        ('лев', ['лев', 'лёв', 'лѣв']),
+        ('венский', ['вѣнскій']),
+    ]
+
+    SUITE_ITEMS = [
+        (
+            it[0], # key
+            [
+                (w, [encode(w)]) # item, value pair
+                for w in it[1]
+            ]
+        )
+        for it in SUITE
+    ]
+
+    SUITE_VALUES = [
+        (
+            it[0], # key
+            [[encode(w)] for w in it[1]]
+        )
+        for it in SUITE
+    ]
+
+    def record_dawg(self):
+        path = data_path("small", "prediction1917-record.dawg")
+        return dawg_python.RecordDAWG(str("=HHHH")).load(path)
+
+    @pytest.mark.parametrize(("word", "prediction"), SUITE)
+    def test_dawg_prediction(self, word, prediction):
+        d = dawg_python.DAWG().load(data_path("small", "prediction1917.dawg"))
         assert d.similar_keys(word, self.REPLACES) == prediction
 
     @pytest.mark.parametrize(("word", "prediction"), SUITE)
